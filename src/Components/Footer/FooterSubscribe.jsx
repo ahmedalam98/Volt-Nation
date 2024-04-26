211d737b3061d8b7160e5814e9a92aa458da72f6
ahmedalam98 <ahmed.alameldeen,me@gmail.com>
2024-04-27 02:12:46 +0300

    Add subscribtion headline

--- src/Components/Footer/FooterSubscribe.jsx
+++ src/Components/Footer/FooterSubscribe.jsx
@@ -3,20 +3,25 @@ import styles from "./FooterSubscribe.module.css";
 const FooterSubscribe = () => {
   return (
     <div className=" bg-[#1B1B1B]">
-      <div className="flex flex-col gap-8 md:gap-0 md:flex-row items-center justify-between h-fit md:h-[100px] mx-10">
+      <div className="flex flex-col gap-8 md:gap-0 md:flex-row items-center justify-between h-fit md:h-[140px] mx-10">
         <p className=" text-md tracking-widest text-gray-400 mt-5 md:mt-0">
           &#169; 2024 by VoltNation{" "}
         </p>
 
-        <div className="flex flex-col md:flex-row justify-center items-center gap-4">
-          <input
-            className="shadow appearance-none border rounded w-[240px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="text"
-            placeholder="Email Address"
-          />
+        <div>
+          <h4 className="text-white text-center mb-4 text-lg tracking-widest">
+            Subsribe To Our Newsletter
+          </h4>
+          <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+            <input
+              className="shadow appearance-none border rounded w-[240px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="Email Address"
+            />
 
-          <button className={styles.subBtn}>Subscribe</button>
+            <button className={styles.subBtn}>Subscribe</button>
+          </div>
         </div>
 
         <div className={`flex gap-5 ${styles.cardsContainer}`}>
