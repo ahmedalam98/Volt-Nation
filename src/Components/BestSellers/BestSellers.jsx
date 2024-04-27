4bd2daedadd5bf98d40c6f56a01c964b42aaf19c
Merna21 <mernadesoky@gmail.com>
2024-04-27 05:48:18 +0300

    feat: :art: Add product card

---
+++ src/Components/BestSellers/BestSellers.jsx
@@ -0,0 +1,69 @@
+import Slider from "react-slick";
+import styles from "./BestSellers.module.css";
+import { useQuery } from "react-query";
+import { getProducts } from "../../api/apiFunctions";
+import Card from "../Card/Card.jsx";
+export default function BestSellers() {
+  const { isLoading, isError, data, error, refetch } = useQuery(
+    ["products"],
+    getProducts
+  );
+  console.log(data?.data);
+  const settings = {
+    infinite: true,
+    // fade: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2500,
+    cssEase: "linear",
+
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div>
+      <Slider {...settings} className={styles.slider}>
+        {data?.data?.slice(20, 30).map((el) => (
+          <Card key={el.id} product={el} />
+        ))}
+      </Slider>
+    </div>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import Hero from "../../Components/Hero/Hero.jsx";
 import GridGallery from "../../Components/HomeGallery/GridGallery/GridGallery.jsx";
 import Pulse from "../../Components/Pulse/Pulse.jsx";
 import Offers from "../../Components/Offers/Offers.jsx";
+import BestSellers from "../../Components/BestSellers/BestSellers.jsx";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <GridGallery />
+      <BestSellers />
       <Pulse />
       <Offers />
     </>
