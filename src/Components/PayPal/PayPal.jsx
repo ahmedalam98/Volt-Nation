6d8aaf00a053afd9a2349a540dfd7b7268b448fa
Sayed <sayedshehata33@gmail.com>
2024-05-04 08:09:27 +0300

    feat: :alien: Handle payment functionality

--- src/Components/PayPal/PayPal.jsx
+++ src/Components/PayPal/PayPal.jsx
@@ -1,34 +1,42 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { checkout } from "../../api/apiFunctions";
+import { useNavigate } from "react-router-dom";
 
 export default function PayPal({ price }) {
+  const navigate = useNavigate();
   const paypal = useRef();
+
   useEffect(() => {
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions, err) => {
-          return actions.order.create({
-            intent: "CAPTURE",
-            purchase_units: [
-              {
-                description: "Your products",
-                amount: {
-                  currency_code: "USD",
-                  value: price,
-                },
+    let paypalButton = window.paypal.Buttons({
+      createOrder: (data, actions, err) => {
+        return actions.order.create({
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              description: "Your products",
+              amount: {
+                currency_code: "USD",
+                value: price,
               },
-            ],
-          });
-        },
-        onApprove: async (data, actions) => {
-          const order = await actions.order.capture();
-          console.log(order);
-        },
-        onError: (err) => {
-          console.log(err);
-        },
-      })
-      .render(paypal.current);
-  });
+            },
+          ],
+        });
+      },
+      onApprove: async (data, actions) => {
+        const order = await actions.order.capture();
+        console.log(order);
+        checkout();
+        navigate("/");
+      },
+      onError: (err) => {
+        console.log(err);
+      },
+    });
+
+    paypalButton.render(paypal.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="w-full mt-5">
       <div ref={paypal}></div>
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://volt-nation.up.railway.app",
+  baseURL: "http://localhost:2024",
 });
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
