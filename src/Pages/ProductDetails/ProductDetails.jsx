30d28b52b18608219590f9b3f2a68a902c4012da
ahmedalam98 <ahmed.alameldeen,me@gmail.com>
2024-05-13 10:46:48 +0300

    fix: :construction: Add project is under development

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function ProductDetails() {
-  return <div className="text-white">ProductDetails</div>;
+  return (
+    <div
+      className="text-white grid place-items-center"
+      style={{ height: "90vh" }}
+    >
+      <h1 className="text-9xl text-center">Project is under development 🚧</h1>
+    </div>
+  );
 }
